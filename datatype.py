9bbef21a0034edfe7bdfc59e273eccab68cec208
RitzTheCoder <ritzthecoder@gmail.com>
2025-03-07 17:49:23 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,8 @@
+a = 5
+print("Type of data of a:", type(a))
+b = 15
+print("Type of data of b:", type(b))
+c = "Ritesh"
+print("Type of data of c:", type(c))
+d = True
+print("Type of data of d:", type(d))
