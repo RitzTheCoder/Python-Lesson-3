ee26a2c1f75146cb6b6c881420a790e3279ac3e6
RitzTheCoder <ritzthecoder@gmail.com>
2025-03-07 18:08:36 +0530

    Add files via upload

---
+++ typecaste.py
@@ -0,0 +1,24 @@
+name = "Ritz"
+age = 13
+is_student = True
+weight = 49.9
+
+print("Name:", name)
+print("Datatype of Name is:", type(name))
+
+print("Age:", age )
+print("Datatype of Age is:", type(age))
+
+print("Name:",is_student)
+print("Datatype of Student is:", type(is_student))
+
+print("Name:",weight)
+print("Datatype of Weight is:", type(weight))
+
+print("\n After Type casting...")
+age=str(age)
+print(age)
+print("Datatype of age is:", type(age))
+weight = int(weight)
+print(weight)
+print("Datatype of weight is:",type(weight))
